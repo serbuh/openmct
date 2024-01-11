fa3cec6abfae3196524d376b46fbe313957ce33b
Buh <serbuh@gmail.com>
2024-01-11 21:00:52 +0200

    Convert telemetry_plugins to the new format

--- telemetry_plugin/CVAS-plugin.js
+++ telemetry_plugin/CVAS-plugin.js
@@ -1,93 +1,82 @@
 
-define([
-    //"./CVASLimitProvider_Test",
-], function (
-    //CVASLimitProvider
-) {
+export default function CVASPlugin() {
+    function get_openmct_interface() {
+        return fetch('../messages_interface/openmct_interface.json').then(function (response) {
+            return response.json();
+        });
 
-    function CVASPlugin() {
-
-        function get_openmct_interface() {
-            return fetch('../messages_interface/openmct_interface.json').then(function (response) {
-                return response.json();
-            });
-
-        }
-
-        // An object provider builds Domain Objects
-        var CVAS_objectProvider = {
-            get: function (identifier) {
-                return get_openmct_interface().then(function (dictionary) {
-                    //console.log("CVAS-dictionary-plugin.js: identifier.key = " + identifier.key);
-                    if (identifier.key === 'CVAS') {
-                        return {
-                            identifier: identifier,
-                            name: dictionary.name,
-                            type: 'folder',
-                            location: 'ROOT'
-                        };
-                    } else {
-                        var measurement = dictionary.measurements.filter(function (m) {
-                            return m.key === identifier.key;
-                        })[0];
-
-                        return {
-                            identifier: identifier,
-                            name: measurement.name,
-                            type: 'CVAS.telemetry',
-                            telemetry: {
-                                values: measurement.values
-                            },
-                            location: 'CVAS.taxonomy:CVAS'
-                        };
-                    }
-                });
-            }
-        };
-
-        // The composition of a domain object is the list of objects it contains, as shown (for example) in the tree for browsing.
-        // Can be used to populate a hierarchy under a custom root-level object based on the contents of a telemetry dictionary.
-        // "appliesTo"  returns a boolean value indicating whether this composition provider applies to the given object
-        // "load" returns an array of Identifier objects (like the channels this telemetry stream offers)
-        var CVAS_compositionProvider = {
-            appliesTo: function (domainObject) {
-                return domainObject.identifier.namespace === 'CVAS.taxonomy'
-                    && domainObject.type === 'folder';
-            },
-            load: function (domainObject) {
-                return get_openmct_interface()
-                    .then(function (dictionary) {
-                        return dictionary.measurements.map(function (m) {
-                            return {
-                                namespace: 'CVAS.taxonomy',
-                                key: m.key
-                            };
-                        });
-                    });
-            }
-        };
-
-        return function install(openmct) {
-            // The addRoot function takes an "object identifier" as an argument
-            openmct.objects.addRoot({
-                namespace: 'CVAS.taxonomy',
-                key: 'CVAS'
-            });
-
-            openmct.objects.addProvider('CVAS.taxonomy', CVAS_objectProvider);
-
-            openmct.composition.addProvider(CVAS_compositionProvider);
-
-            //openmct.telemetry.addProvider(new CVASLimitProvider());
-
-            openmct.types.addType('CVAS.telemetry', {
-                name: 'CVAS Telemetry Point',
-                description: 'Telemetry of CVAS',
-                cssClass: 'icon-telemetry'
-            });
-        };
     }
 
-    return CVASPlugin;
-});
+    // An object provider builds Domain Objects
+    var CVAS_objectProvider = {
+        get: function (identifier) {
+            return get_openmct_interface().then(function (dictionary) {
+                //console.log("CVAS-dictionary-plugin.js: identifier.key = " + identifier.key);
+                if (identifier.key === 'CVAS') {
+                    return {
+                        identifier: identifier,
+                        name: dictionary.name,
+                        type: 'folder',
+                        location: 'ROOT'
+                    };
+                } else {
+                    var measurement = dictionary.measurements.filter(function (m) {
+                        return m.key === identifier.key;
+                    })[0];
 
+                    return {
+                        identifier: identifier,
+                        name: measurement.name,
+                        type: 'CVAS.telemetry',
+                        telemetry: {
+                            values: measurement.values
+                        },
+                        location: 'CVAS.taxonomy:CVAS'
+                    };
+                }
+            });
+        }
+    };
+
+    // The composition of a domain object is the list of objects it contains, as shown (for example) in the tree for browsing.
+    // Can be used to populate a hierarchy under a custom root-level object based on the contents of a telemetry dictionary.
+    // "appliesTo"  returns a boolean value indicating whether this composition provider applies to the given object
+    // "load" returns an array of Identifier objects (like the channels this telemetry stream offers)
+    var CVAS_compositionProvider = {
+        appliesTo: function (domainObject) {
+            return domainObject.identifier.namespace === 'CVAS.taxonomy'
+                && domainObject.type === 'folder';
+        },
+        load: function (domainObject) {
+            return get_openmct_interface()
+                .then(function (dictionary) {
+                    return dictionary.measurements.map(function (m) {
+                        return {
+                            namespace: 'CVAS.taxonomy',
+                            key: m.key
+                        };
+                    });
+                });
+        }
+    };
+
+    return function install(openmct) {
+        // The addRoot function takes an "object identifier" as an argument
+        openmct.objects.addRoot({
+            namespace: 'CVAS.taxonomy',
+            key: 'CVAS'
+        });
+
+        openmct.objects.addProvider('CVAS.taxonomy', CVAS_objectProvider);
+
+        openmct.composition.addProvider(CVAS_compositionProvider);
+
+        //openmct.telemetry.addProvider(new CVASLimitProvider());
+
+        openmct.types.addType('CVAS.telemetry', {
+            name: 'CVAS Telemetry Point',
+            description: 'Telemetry of CVAS',
+            cssClass: 'icon-telemetry'
+        });
+    };
+}
--- telemetry_plugin/historical-telemetry-plugin.js
+++ telemetry_plugin/historical-telemetry-plugin.js
@@ -2,45 +2,35 @@
  * Basic historical telemetry plugin.
  */
 
-define([
 
-], function (
+export default function HistoricalTelemetryPlugin(desired_domain_object_type, serverURL, IP) {
+    return function install(openmct) {
+        //var desired_domain_object_type = 'TFLEX.telemetry';
+        var port = 16969;
+        var provider = {
+            supportsRequest: function (domainObject) {
+                return domainObject.type === desired_domain_object_type;
+            },
+            request: function (domainObject, options) {
+                var url = 'http://' + IP + ':' + port + serverURL
+                    + domainObject.identifier.key
+                    + '?start=' + options.start
+                    + '&end=' + options.end;
+                console.log('historical-telemetry-plugin.js: send request = ' + url);
 
-) {
+                //http gibts nicht mehr!!!!!!!!!!!!!!!!!
+                return fetch(url).then(function (resp) {
+                    console.log(resp);
 
-    function HistoricalTelemetryPlugin(desired_domain_object_type, serverURL, IP) {
-    //function HistoricalTelemetryPlugin() {
-
-        return function install(openmct) {
-            //var desired_domain_object_type = 'TFLEX.telemetry';
-            var port = 16969;
-            var provider = {
-                supportsRequest: function (domainObject) {
-                    return domainObject.type === desired_domain_object_type;
-                },
-                request: function (domainObject, options) {
-                    var url = 'http://' + IP + ':' + port + serverURL
-                        + domainObject.identifier.key
-                        + '?start=' + options.start
-                        + '&end=' + options.end;
-                    console.log('historical-telemetry-plugin.js: send request = ' + url);
-
-                    //http gibts nicht mehr!!!!!!!!!!!!!!!!!
-                    return fetch(url).then(function (resp) {
-                        console.log(resp);
-
-                        return resp.json();
-                    });
-                    // return http.get(url)
-                    //     .then(function (resp) {
-                    //         return resp.data;
-                    //     });
-                }
-            };
-
-            openmct.telemetry.addProvider(provider);
+                    return resp.json();
+                });
+                // return http.get(url)
+                //     .then(function (resp) {
+                //         return resp.data;
+                //     });
+            }
         };
-    }
 
-    return HistoricalTelemetryPlugin;
-});
+        openmct.telemetry.addProvider(provider);
+    };
+}
