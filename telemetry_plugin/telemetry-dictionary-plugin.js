253fe31341cf58ad92e0894808115fb516050dd4
Buh <serbuh@gmail.com>
2024-02-23 21:02:19 +0200

    put object provider and composition provider inside the functions

--- telemetry_plugin/telemetry-dictionary-plugin.js
+++ telemetry_plugin/telemetry-dictionary-plugin.js
@@ -7,65 +7,7 @@ export default function TelemetryDictionaryPlugin() {
         // return fetch('../telemetry_plugin/openmct_interface.json').then(function (response) {
         //     return response.json();
         // });
-
     }
-
-    // An object provider builds Domain Objects
-    var CVAS_objectProvider = {
-        get: function (identifier) {
-            return get_openmct_interface().then(function (dictionary) {
-                if (identifier.key === 'RootObject') {
-                    // JSON.stringify(identifier, null, 4)
-                    // console.log("Getting root " + dictionary.name + " identifier: " + identifier.namespace + " -> " + identifier.key);
-                    return {
-                        identifier: identifier,
-                        name: dictionary.name,
-                        type: 'folder',
-                        location: 'ROOT'
-                    };
-                } else {
-                    var measurement = dictionary.measurements.filter(function (m) {
-                        return m.key === identifier.key;
-                    })[0];
-                    // console.log("Getting measurement " + measurement.name + " identifier: " + identifier.namespace + " -> " + identifier.key)
-
-                    return {
-                        identifier: identifier,
-                        name: measurement.name,
-                        type: 'TelemetryDomainObject',
-                        telemetry: {
-                            values: measurement.values
-                        },
-                        location: 'TelemetryMainspace:RootObject'
-                    };
-                }
-            });
-        }
-    };
-
-    // The composition of a domain object is the list of objects it contains, as shown (for example) in the tree for browsing.
-    // Can be used to populate a hierarchy under a custom root-level object based on the contents of a telemetry dictionary.
-    // "appliesTo"  returns a boolean value indicating whether this composition provider applies to the given object
-    // "load" returns an array of Identifier objects (like the channels this telemetry stream offers)
-    var CVAS_compositionProvider = {
-        appliesTo: function (domainObject) {
-            // console.log("domainObject " + domainObject.name + " type " + domainObject.type)
-            return domainObject.identifier.namespace === 'TelemetryMainspace'
-                && domainObject.type === 'folder';
-        },
-        load: function (domainObject) {
-            return get_openmct_interface()
-                .then(function (dictionary) {
-                    return dictionary.measurements.map(function (m) {
-                        // console.log("COMPOSITION load " + m.key)
-                        return {
-                            namespace: 'TelemetryMainspace',
-                            key: m.key
-                        };
-                    });
-                });
-        }
-    };
 
     return function install(openmct) {
         // The addRoot function takes an "object identifier" as an argument
@@ -73,14 +15,72 @@ export default function TelemetryDictionaryPlugin() {
             namespace: 'TelemetryMainspace',
             key: 'RootObject'
         });
+        
+        // An object provider builds Domain Objects
+        openmct.objects.addProvider('TelemetryMainspace', {
+            get: function (identifier) {
+                return get_openmct_interface().then(function (dictionary) {
+                    // console.log(JSON.stringify(identifier, null, 4))
+                    // console.log("Getting root " + dictionary.name + " identifier: " + identifier.namespace + " -> " + identifier.key);
+                    if (identifier.key === 'RootObject') {
+                        return {
+                            identifier: identifier,
+                            name: dictionary.name,
+                            type: 'folder',
+                            location: 'ROOT'
+                        };
+                    // } else if (identifier.key === 'Somekey') {
+                    //     console.log("FOLDER!" + JSON.stringify(identifier, null, 4))
+                    //     return {
+                    //         identifier: identifier,
+                    //         name: "PAPKA",
+                    //         type: 'folder',
+                    //         location: 'TelemetryMainspace:RootObject'
+                    //     };
+                    } else {
+                        var measurement = dictionary.measurements.filter(function (m) {
+                            return m.key === identifier.key;
+                        })[0];
+    
+                        return {
+                            identifier: identifier,
+                            name: measurement.name,
+                            type: 'TelemetryDomainObject',
+                            telemetry: {
+                                values: measurement.values
+                            },
+                            location: 'TelemetryMainspace:RootObject'
+                        };
+                    }
+                });
+            }
+        });
 
-        openmct.objects.addProvider('TelemetryMainspace', CVAS_objectProvider);
-
-        openmct.composition.addProvider(CVAS_compositionProvider);
+        // Composition provider
+        openmct.composition.addProvider({
+            appliesTo: function (domainObject) {
+                // console.log("domainObject " + JSON.stringify(domainObject, null, 4))
+                return domainObject.identifier.namespace === 'TelemetryMainspace'
+                    && domainObject.type === 'folder';
+                    // && domainObject.identifier.key === 'RootObject';
+            },
+            load: function (domainObject) {
+                return get_openmct_interface()
+                    .then(function (dictionary) {
+                        return dictionary.measurements.map(function (m) {
+                            // console.log("COMPOSITION load " + m.key)
+                            return {
+                                namespace: 'TelemetryMainspace',
+                                key: m.key
+                            };
+                        });
+                    });
+            }
+        });
 
         openmct.types.addType('TelemetryDomainObject', {
-            name: 'CVAS Telemetry Point',
-            description: 'Telemetry of CVAS',
+            name: 'Telemetry Point',
+            description: 'Telemetry',
             cssClass: 'icon-telemetry',
         });
     };
