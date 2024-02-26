1480a1ccdacd141d1545b9f294559dd04b43a70e
Buh <serbuh@gmail.com>
2024-02-26 11:45:22 +0200

    looks like nesting is working

--- telemetry_plugin/telemetry-dictionary-plugin.js
+++ telemetry_plugin/telemetry-dictionary-plugin.js
@@ -1,54 +1,4 @@
 import dictionary_src from './openmct_interface.json' assert { type: 'json' };
-// window.dictionary = dictionary_src
-
-class SuperObjectProvider {
-    #get_openmct_interface() {
-        return Promise.resolve(dictionary_src)
-    }
-
-    constructor(openmct) {
-        this.openmct = openmct;
-    }
-
-    async get(identifier) {
-        //console.log("GET! " + identifier.key);//JSON.stringify(identifier, null, 4))
-        return this.#get_openmct_interface().then(function (dictionary) {
-            // console.log(JSON.stringify(identifier, null, 4))
-            if (identifier.key === 'RootObject') {
-                // Create root folder
-                return {
-                    identifier: identifier,
-                    name: "PredefinedTelemetry",
-                    type: 'folder',
-                    location: 'ROOT'
-                };
-            } else {
-                var measurement = dictionary.measurements.filter(function (m) {
-                    return m.key === identifier.key;
-                })[0];
-                if (measurement.node_type === 'folder') {
-                    //console.log("FOLDER " + identifier.key)
-                    return {
-                        identifier: identifier,
-                        name: measurement.name,
-                        type: 'folder',
-                        location: 'TelemetryMainspace:RootObject'
-                    };    
-                } else {
-                    return {
-                        identifier: identifier,
-                        name: measurement.name,
-                        type: 'TelemetryDomainObject',
-                        telemetry: {
-                            values: measurement.values
-                        },
-                        location: 'TelemetryMainspace:Folder_0_key'
-                    };
-                }
-            }
-        });
-    }
-}
 
 export default function TelemetryDictionaryPlugin() {
     function get_openmct_interface() {
@@ -59,31 +9,67 @@ export default function TelemetryDictionaryPlugin() {
         // The addRoot function takes an "object identifier" as an argument
         openmct.objects.addRoot({
             namespace: 'TelemetryMainspace',
-            key: 'RootObject'
+            key: 'RootFolder'
         });
-        
-        const objectProvider = new SuperObjectProvider(
-            openmct
-        );
 
         // An object provider builds Domain Objects
-        openmct.objects.addProvider('TelemetryMainspace', objectProvider);
+        openmct.objects.addProvider('TelemetryMainspace', {
+            get: function(identifier) {
+                //console.log("GET! " + identifier.key);//JSON.stringify(identifier, null, 4))
+                return get_openmct_interface().then(function (dictionary) {
+                    // console.log(JSON.stringify(identifier, null, 4))
+                    if (identifier.key === 'RootFolder') {
+                        // Create root folder
+                        return {
+                            identifier: identifier,
+                            name: "PredefinedTelemetry",
+                            type: 'folder',
+                            location: 'ROOT'
+                        };
+                    } else {
+                        var measurement = dictionary.measurements.filter(function (m) {
+                            return m.key === identifier.key;
+                        })[0];
+                        // Asuming that telemetry entries has 'value' fields. Otherwise treating as a folder
+                        if (measurement.hasOwnProperty('values')) {
+                            // Telemetry entry
+                            return {
+                                identifier: identifier,
+                                name: measurement.name,
+                                type: 'TelemetryDomainObject',
+                                telemetry: {
+                                    values: measurement.values
+                                },
+                                location: 'TelemetryMainspace:' + measurement.nested_under
+                            };
+                        } else {
+                            // Folder
+                            return {
+                                identifier: identifier,
+                                name: measurement.name,
+                                type: 'folder',
+                                location: 'TelemetryMainspace:' + measurement.nested_under
+                            };    
+                        }
+                    }
+                });
+            }
+        });
 
         // Composition provider
         openmct.composition.addProvider({
             appliesTo: function (domainObject) {
-                
-                // const predicate = domainObject.key === "RootObject"
-                // console.log("AppliesTo? "+JSON.stringify(domainObject, null, 4) + "\n " + predicate)
                 return domainObject.identifier.namespace === 'TelemetryMainspace'
                     && domainObject.type === 'folder';
-                    // && domainObject.identifier.key === "RootObject";
             },
             load: function (domainObject) {
+                // console.log("Load for " + domainObject.identifier.key)
                 return get_openmct_interface()
                     .then(function (dictionary) {
-                        return dictionary.measurements.map(function (m) {
-                            // console.log("COMPOSITION load " + m.key)
+                        return dictionary.measurements
+                            .filter(m => domainObject.identifier.key === m.nested_under)
+                            .map(function (m) {
+                            // console.log("   load " + domainObject.identifier.key + " -> " + m.key + " under " + m.nested_under)
                             return {
                                 namespace: 'TelemetryMainspace',
                                 key: m.key
